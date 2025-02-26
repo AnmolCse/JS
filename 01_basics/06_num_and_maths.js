27f8777c0c81d527b03b9f06c41046b6448bf97e
Anmol Verma <anmolverma141315@gmail.com>
2025-02-26 17:47:36 +0000

    done

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,40 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.66
+
+// console.log(otherNumber.toPrecision(3))
+
+const num = 1000000
+// console.log(num.toLocaleString('en-In'))
+
+// ************************************ Maths ***************************************************
+
+// It is a library which came by default
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2))
+// console.log((Math.floor(3.8)));
+
+// console.log(Math.max(2,6,1,4))
+
+// console.log(Math.random());
+// value lies between 0 and 1
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
+
+
+
