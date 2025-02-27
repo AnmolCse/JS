3d340c4f3661e327867f40bc4cb2d9e301d0235f
Anmol Verma <anmolverma141315@gmail.com>
2025-02-27 17:41:35 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,15 +2,37 @@
 // Object.create
 
 // object literals
+const mySym = Symbol("key1")
 const JsUser = {
     name : "Anmol",       // system treats key as string itself 
-    age : 20, 
+    age : 20,
+    [mySym] : "MyKey1",   // Symbol is defined like this
     location : "Mhow",
-    email : "ag@ag.com"
-    isLoggedIn : false
+    email : "ag@ag.com",
+    isLoggedIn : false,
     lastLogginDays : ["Monday", "Friday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// two ways to access objects
 
+// console.log(JsUser[mySym]); // It  is the only way to access symbol in objects
+
+JsUser.email = "ag@chatgpt.com"
+// console.log(JsUser.email);
+// Object.freeze(JsUser)
+// JsUser.email = "ag@word.com"
+// console.log(JsUser.email);
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");   
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);   // this is used to take reference of the same object
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
